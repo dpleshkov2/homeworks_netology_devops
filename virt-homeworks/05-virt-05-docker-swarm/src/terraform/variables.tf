615acf1845ee2736f45ff0b09a127826beef0f32
dmitry-pleshkov <pleshkoff@mail.ru>
2023-03-24 10:18:16 +0300

    05-virt-05-docker-swarm task3 src configured and assets added.

--- virt-homeworks/05-virt-05-docker-swarm/src/terraform/variables.tf
+++ virt-homeworks/05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1g5fc6bh167g4gg3m3h"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1gm911229ahmtfer3bh"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd8072r40e36d35im6he"
 }
