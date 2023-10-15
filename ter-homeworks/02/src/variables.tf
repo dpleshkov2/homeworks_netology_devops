062b3bb92ce5c09593b307433269fed6248d5f57
dpleshkov <pleshkoff@mail.ru>
2023-10-16 00:56:09 +0300

    Tasks 1, 2 completed

--- ter-homeworks/02/src/variables.tf
+++ ter-homeworks/02/src/variables.tf
@@ -39,3 +39,29 @@ variable "vms_ssh_root_key" {
   default     = "<your_ssh_ed25519_key>"
   description = "ssh-keygen -t ed25519"
 }
+
+###yandex_compute_image vars
+variable "vm_web_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "yandex_compute_image family"
+}
+
+###yandex_compute_instance vars
+variable "vm_web_cores" {
+  type        = number
+  default     = "2"
+  description = "yandex_compute_instance cores"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default     = "1"
+  description = "yandex_compute_instance memory"
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  default     = "20"
+  description = "yandex_compute_instance core_fraction"
+}
